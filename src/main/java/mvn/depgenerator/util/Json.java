34d971c5f8379e6470c4d7c7c60f1fef350b3938
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-06-22 18:40:07 -0400

    Annotate Json class with UtilityClass

--- src/main/java/mvn/depgenerator/util/Json.java
+++ src/main/java/mvn/depgenerator/util/Json.java
@@ -1,10 +1,12 @@
 package mvn.depgenerator.util;
 
+import lombok.experimental.UtilityClass;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
+@UtilityClass
 public final class Json {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
